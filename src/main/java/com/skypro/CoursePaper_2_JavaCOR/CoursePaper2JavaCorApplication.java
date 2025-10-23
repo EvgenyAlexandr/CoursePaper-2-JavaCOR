9a5caa652d2505360710237541f4784bb7a683a9
EvgenyAlexand <bragin-e@mail.ru>
2025-10-23 22:19:24 +0700

    Init commit

---
+++ src/main/java/com/skypro/CoursePaper_2_JavaCOR/CoursePaper2JavaCorApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.CoursePaper_2_JavaCOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaper2JavaCorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaper2JavaCorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/CoursePaper_2_JavaCOR/CoursePaper2JavaCorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.CoursePaper_2_JavaCOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaper2JavaCorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
