b9fb749755d75378402bc39cfb3cd9f4ab833de3
EvgenyAlexand <bragin-e@mail.ru>
2025-10-23 22:56:48 +0700

    New branch - Курсовая работа - JavaCOR - В процессе выполнения.

---
+++ src/main/java/com/skypro/CoursePaper_2_JavaCOR/controller/ExamController.java
@@ -0,0 +1,4 @@
+package com.skypro.CoursePaper_2_JavaCOR.controller;
+
+public class ExamController {
+}
---
+++ src/main/java/com/skypro/CoursePaper_2_JavaCOR/service/JavaQuestionService.java
@@ -0,0 +1,4 @@
+package com.skypro.CoursePaper_2_JavaCOR.service;
+
+public class JavaQuestionService {
+}
