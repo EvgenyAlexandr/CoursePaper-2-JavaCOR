915ee25c2aab81d03c6919f215c99eb82d6084ff
EvgenyAlexand <bragin-e@mail.ru>
2025-10-26 17:51:22 +0700

    Курсовая работа - JavaCOR - В процессе выполнения.

--- src/main/java/com/skypro/CoursePaper_2_JavaCOR/domain/Question.java
+++ src/main/java/com/skypro/CoursePaper_2_JavaCOR/domain/Question.java
@@ -1,4 +1,19 @@
 package com.skypro.CoursePaper_2_JavaCOR.domain;
 
 public class Question {
+    private final String question;  // Вопрос
+    private final String answer;    // Ответ
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
 }
